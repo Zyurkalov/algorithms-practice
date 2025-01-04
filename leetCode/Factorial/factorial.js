e0fa3c06dcf3c7a5e716ace349c200ad72e1fee2
Zyurkalov <Zyurkalov@gmail.com>
2025-01-04 18:51:59 +0500

    added: factorial and memo

---
+++ leetCode/Factorial/factorial.js
@@ -0,0 +1,13 @@
+const factorial = function() {
+    const memo = {}
+
+    return function getFactorial(f) {
+        if(f<=1) return 1
+        if(memo.f) return memo.f 
+    
+        memo.f = f * getFactorial(f-1)
+        return memo.f
+    }
+}();
+
+console.log(factorial(5)) //120 
